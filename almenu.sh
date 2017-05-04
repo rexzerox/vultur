f854198ef73181da80fc2fb34a2cd945e59d951b
yusuf-ardiansyah <yusuf-ardiansyah@users.noreply.github.com>
2017-05-05 06:53:35 +0700

    Update almenu.sh

--- almenu.sh
+++ almenu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo ""
-echo "SELAMAT DATANG DI INSTALLER ALL MENU VPS By YUSUF ARDIANSYAH"
+echo "SELAMAT DATANG DI INSTALLER ALL MENU VPS By YUSUF-ARDIANSYAH"
 echo ""
 echo "Instalasi aplikasi pewarna text"
 # aplikasi
@@ -11,26 +11,26 @@ echo "Instalasi file pewarna text sysrem"
 # text sistem warna
 cd
 rm -rf .bashrc
-wget https://raw.githubusercontent.com/deeniedoank/autoscript2/master/text%20warna/.bashrc
+wget https://raw.githubusercontent.com/yusuf-ardiansyah/vultur/master/text%20warna/.bashrc
 echo "Instalasi all menu PREMIUM"
 
 # menu
 cd
-wget https://raw.githubusercontent.com/deeniedoank/autoscript2/master/menu/menu
+wget https://raw.githubusercontent.com/yusuf-ardiansyah/vultur/master/menu/menu
 mv ./menu /usr/local/bin/menu
 chmod +x /usr/local/bin/menu
 
 echo "Instalasi motd"
 #motd
 cd
-wget "https://raw.githubusercontent.com/deeniedoank/autoscript2/master/menu/motd"
+wget "https://raw.githubusercontent.com/yusuf-ardiansyah/vultur/master/menu/motd"
 mv ./motd /etc/motd
 
 echo "Instalasi speedtest"
 #speedtest
 cd
 apt-get install python
-wget -O speedtest.py "https://raw.githubusercontent.com/deeniedoank/autoscript2/master/menu/speedtest.py"
+wget -O speedtest.py "https://raw.githubusercontent.com/yusuf-ardiansyah/vultur/master/menu/speedtest.py"
 chmod +x speedtest.py
 
 echo "Selesai...!!!";
